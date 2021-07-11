186321244b9877505d7eb7e1ea43abc56b05a449
yuyunou <yuyunou@126.com>
2021-07-11 17:12:58 +0800

    可变参宏定义

--- va_args.c
+++ va_args.c
@@ -1,24 +1,30 @@
+#include <stdio.h>
 #include <stdarg.h>
 
 
-void log_printf(char format, ...)
+void log_printf(char *format, ...)
 {
-    va_arg va;
-    va_start();
-    
-    va_end();
-
-
+    va_list va;
+    char buf[256] = {0};
+    va_start(va, format);
+    vsprintf(buf, format, va);
+    va_end(va);
+    printf(buf, buf);
 }
 
 
 #define LOG_PRINTF(tar, lev , format, ...)\
     do\
     {\
-        log_printf(format);\
+        log_printf(format, __VA_ARGS__);\
     }while(0)
 
 
+int main()
 
+{
+    int a = 12;
+    LOG_PRINTF(0 , 0, "yyo tttt %d", a);
 
-
+    //log_printf("yyo tttt %d", a);
+}
