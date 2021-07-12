d17f30bad82762061838c13d2c24f2675435028d
yuyunou <yuyunou@126.com>
2021-07-12 20:25:10 +0800

    增加可变参函数demo

--- va_args.c
+++ va_args.c
@@ -20,11 +20,32 @@ void log_printf(char *format, ...)
     }while(0)
 
 
+
+
+int sum(int a, ...)
+{
+    int sum = 0;
+    va_list va;
+    va_start(va, a);
+
+    while (a--)
+    {
+        sum += va_arg(va, int);
+    }
+
+    va_end(va);
+
+    return sum;
+
+}
+
+
+
 int main()
 
 {
     int a = 12;
-    LOG_PRINTF(0 , 0, "yyo tttt %d", a);
+    LOG_PRINTF(0 , 0, "yyo tttt %d", sum(4, 2 ,3 , 1, 77));
 
     //log_printf("yyo tttt %d", a);
 }
